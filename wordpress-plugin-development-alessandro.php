0f3fa47370816567f34d3c83b6e15993274836f6
rahneverd <rahneverd@gmail.com>
2025-03-16 22:27:02 +0500

    added author and plugin info

---
+++ wordpress-plugin-development-alessandro.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package WordPressPluginDevelopmentAlessandro
+ */
+/*
+Plugin Name: WordPress Plugin Development - Alessandro
+Plugin URI: http://github.com/rahneverd/wordpress-plugin-development-alessandro
+Description: A custom WordPress plugin developed by Rahneverd - Alessandro Tutorials.
+Version: 1.0.0
+Author: Muhammad Owais Nizami - "Rahneverd"
+Author URI: http://github.com/rahneverd
+License: GPLv2 or later
+Text Domain: wppd-alessandro-rahneverd
+*/
