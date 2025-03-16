096492309f7eb38c277129c7967865c316fab439
rahneverd <rahneverd@gmail.com>
2025-03-16 22:45:50 +0500

    added security

--- wordpress-plugin-development-alessandro.php
+++ wordpress-plugin-development-alessandro.php
@@ -29,3 +29,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, see
 <https://www.gnu.org/licenses/>.
 */
+
+if (!defined('ABSPATH')) {
+  die;
+}
